302a8cf928de5c20b8fadb93bdbebfc7c76b9b2d
Deus Nsenga <deusnsenga@gmail.com>
2020-07-15 16:12:03 +0000

    This is the testing module for week 5

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,11 +13,98 @@
 // limitations under the License.
 
 package com.google.sps;
-
+ 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // handle outside cases
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+          return Arrays.asList();
+    }
+
+    if (request.getAttendees().size() == 0){
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    Collection<TimeRange> results = new ArrayList<TimeRange>();
+    // Find event that are attended by at least one of our people
+    Collection<String> attendees = request.getAttendees();
+    Collection<String> optionalAttendees = request.getOptionalAttendees();
+    
+    Collection<Event> relevantEvents = new HashSet<>();
+    Collection<Event> relevantEventsForMandatoryAttendees = new HashSet<>();
+
+    for (Event e : events) {
+      if (!Collections.disjoint(e.getAttendees(), attendees)) {
+          relevantEvents.add(e);
+          relevantEventsForMandatoryAttendees.add(e);  
+      }
+      else if (!Collections.disjoint(e.getAttendees(), optionalAttendees)) {
+          relevantEvents.add(e);
+      }
+    }
+
+    if (relevantEvents.size() == 0){
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    ArrayList<TimeRange> relevantEventsTimeRange = new ArrayList<TimeRange>();
+    ArrayList<TimeRange> relevantEventsForMandatoryAttendeesTimeRange = new ArrayList<TimeRange>();
+    for (Event e: relevantEvents){
+        relevantEventsTimeRange.add(e.getWhen());
+    }
+
+    for (Event e: relevantEventsForMandatoryAttendees){
+        relevantEventsForMandatoryAttendeesTimeRange.add(e.getWhen());
+    }
+
+    results = queryHelper(relevantEventsTimeRange, request.getDuration());
+
+    if (results.size() != 0) return results;
+
+    return queryHelper(relevantEventsForMandatoryAttendeesTimeRange, request.getDuration());
+
+
+    
+  }
+
+  private Collection<TimeRange> queryHelper(ArrayList<TimeRange> relevantEventsTimeRange, long meetingDuration) {
+
+    Collection<TimeRange> results = new ArrayList<TimeRange>();
+
+
+    Collections.sort(relevantEventsTimeRange, TimeRange.ORDER_BY_START);
+    TimeRange firstWindow = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, relevantEventsTimeRange.get(0).start(), false);
+
+    if (firstWindow.duration() >= meetingDuration){
+        results.add(firstWindow);
+    }
+
+    int currentEnd = relevantEventsTimeRange.get(0).end();
+    for (int i = 1; i < relevantEventsTimeRange.size(); i++){
+        if (currentEnd < relevantEventsTimeRange.get(i).start() ) {
+            TimeRange tempTimeRange = TimeRange.fromStartEnd(currentEnd, relevantEventsTimeRange.get(i).start(), false);
+
+            if (tempTimeRange.duration() >= meetingDuration){
+                results.add(tempTimeRange);
+            }
+        }
+        if (relevantEventsTimeRange.get(i).end() > currentEnd) currentEnd = relevantEventsTimeRange.get(i).end();
+    }
+
+    Collections.sort(relevantEventsTimeRange, TimeRange.ORDER_BY_END);
+    TimeRange lastWindow = TimeRange.fromStartEnd(relevantEventsTimeRange.get(relevantEventsTimeRange.size()-1).end(), TimeRange.END_OF_DAY, true);
+
+    if (lastWindow.duration() >= meetingDuration){
+        results.add(lastWindow);
+    }
+
+    return results;
   }
 }
